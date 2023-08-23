82b0f7f598cec0bb289d20b8af01a5e25876f21d
Teebra <125788246+Teebra@users.noreply.github.com>
2023-08-23 18:26:58 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 import pytest
-import time
-from app import app, board, player_symbol, bot_symbol, current_player, is_game_over, bot_move, player_move, restart_game
+from app import (app, board, player_symbol, bot_symbol,
+                 current_player, is_game_over, bot_move,
+                 player_move, restart_game)
+
 
 @pytest.fixture
 def client():
@@ -8,10 +10,12 @@ def client():
     with app.test_client() as client:
         yield client
 
+
 def test_index(client):
     response = client.get('/')
     assert response.status_code == 200
     assert b'Tic Tac Toe' in response.data
+
 
 def test_make_move(client):
     position = 0
@@ -22,8 +26,11 @@ def test_make_move(client):
     assert 'current_player' in data
     assert 'game_over' in data
     assert data['board'][position] == player_symbol
-    assert data['current_player'] == bot_symbol or data['current_player'] == player_symbol
+    assert (data['current_player'] == bot_symbol or
+            data['current_player'] == player_symbol)
+
     assert isinstance(data['game_over'], bool)
+
 
 def test_restart(client):
     response = client.post('/restart')
@@ -36,9 +43,11 @@ def test_restart(client):
     assert data['current_player'] == player_symbol
     assert isinstance(data['game_over'], bool)
 
+
 def test_is_game_over():
     # Test when the game is not over
     assert not is_game_over()
+
 
 def test_bot_move():
     # Test when there are no available moves
@@ -47,6 +56,7 @@ def test_bot_move():
     board[6] = board[7] = board[8] = player_symbol
     bot_move()
     assert all(cell != "" for cell in board)
+
 
 def test_player_move():
     position = 0
@@ -59,6 +69,7 @@ def test_player_move():
     board[position] = player_symbol
     updated_board = player_move(position)
     assert updated_board[position] == player_symbol
+
 
 def test_restart_game():
     # Set up the initial game state
